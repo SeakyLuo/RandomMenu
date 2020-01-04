83938b78b7a1784973b054d9fedce918eeb341dc
SeakyLuo <haitianluo@umail.ucsb.edu>
2020-01-04 10:04:59 +0800

    pull to refresh

--- app/src/main/java/personalprojects/seakyluo/randommenu/NavigationFragment.java
+++ app/src/main/java/personalprojects/seakyluo/randommenu/NavigationFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -36,7 +37,11 @@ public class NavigationFragment extends Fragment {
         title_text_view = view.findViewById(R.id.title_text_view);
         FloatingActionButton fab = view.findViewById(R.id.navi_fab);
         fab.setOnClickListener(v -> EditFood(null));
-
+        SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            SetData();
+            swipeRefreshLayout.setRefreshing(false);
+        });
         view.findViewById(R.id.search_button).setOnClickListener(v -> {
             startActivity(new Intent(getContext(), SearchActivity.class));
             getActivity().overridePendingTransition(R.anim.push_right_in, 0);
